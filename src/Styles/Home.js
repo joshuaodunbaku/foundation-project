91851a220060cdb1896568a9c4067c93a29a85b4
joshuaodunbaku <joshuaodunbaku@gmail.com>
2025-02-22 12:54:43 +0100

    updated about page

--- src/Styles/Home.js
+++ src/Styles/Home.js
@@ -88,46 +88,4 @@ export const Wrapper = styled.div`
   #section-5 {
     max-width: 600px;
   }
-
-  #volunteer-section {
-    background-color: #f8f9fa; /* Light background for the section */
-  }
-
-  .volunteer-card {
-    background-color: white;
-    border-radius: 12px;
-    padding: 20px;
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1); /* Slight shadow to lift the div */
-    transition: transform 0.3s ease-in-out; /* Smooth hover effect */
-    text-align: center;
-  }
-
-  .volunteer-card:hover {
-    transform: translateY(-10px); /* Lift the card on hover */
-  }
-
-  .volunteer-card h4 {
-    font-size: 1.25rem;
-    font-weight: 600;
-  }
-
-  .volunteer-card p {
-    color: #666;
-    font-size: 1rem;
-  }
-
-  .volunteer-img-container {
-    width: 150px;
-    height: 150px;
-    margin: 0 auto;
-    border-radius: 50%;
-    overflow: hidden;
-    margin-bottom: 15px;
-  }
-
-  .volunteer-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
 `;
